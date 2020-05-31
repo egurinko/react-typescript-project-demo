d2f4334260f268ebc1d721de710efa3bfbdc0908
egurinko <egctoru@gmail.com>
2020-05-31 15:06:30 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# 2020 Settings of React TypeScript Project with webpack and Babel
+
+https://medium.com/@egctoru/2020-settings-of-react-typescript-project-with-webpack-and-babel-403c92feaa06
+
+## Tech Stacks
+
+- React
+- TypeScript
+- Webpack
+- Babel
+
+## Command
+
+- Start dev server
+
+```
+yarn dev
+```
+
+- Build
+
+```
+yarn build
+```
+
+- Check types
+
+```
+yarn check-type
+```
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/Index.tsx',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist')
+  }
+}
